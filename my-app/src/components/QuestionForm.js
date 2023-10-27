0bc48fedbac07de47fe3f354d7991a3df489bd6f
Lâm Tiên Hưng <Hungltse170216@fpt.edu.vn>
2023-10-27 18:26:58 +0700

    Update QuestionForm.js
    Change form of JSON data

--- my-app/src/components/QuestionForm.js
+++ my-app/src/components/QuestionForm.js
@@ -1,62 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { ClearAllAnswers, ClearSelectedAnswer, LoadAnswers, SaveAnswer } from '../layouts/ScreenQuiz/QuizService';
-import './QuestionForm.scss'
+import './QuestionForm.scss';
+
 export default function QuestionForm(props) {
-    // const nav = useNavigate();
-    const [selectedAnswers, SetSelectedAnswers] = useState({});
-    const HandleAnswerClick = (quizId, answerId) => {
-        SetSelectedAnswers((prev) => ({
-            ...prev, [quizId]: answerId
-        }))
-    }
-    LoadAnswers(SetSelectedAnswers);
+    const [selectedAnswers, setSelectedAnswers] = useState({});
+
+    const handleAnswerClick = (quizId, answerId) => {
+        setSelectedAnswers((prev) => ({
+            ...prev,
+            [quizId]: answerId
+        }));
+    };
+
+    LoadAnswers(setSelectedAnswers);
     SaveAnswer(selectedAnswers);
+
     return (
-        <div className='question-form'>
-            <h1 className='time'>
+        <div className="question-form">
+            <h1 className="time">
                 Time: <label>10:00</label>
             </h1>
-            {
-                props.quizData.map((v, i) => (
-
-                    <div key={v.id}>
-                        <h2 className='form__header'>Quizz</h2>
-                        <div className='form__body'>
-                            {
-                                v.lsQuizz.map((quiz, j) => (
-                                    <div key={quiz.id}>
-                                        <p className='question'>Question {j + 1}: {quiz.content}</p>
-                                        <ul className='answers' style={{listStyle:'none'}}>
-                                            {
-                                                quiz.answer.map((answer, k) => (
-                                                    <li className='answer'
-                                                        key={answer.id}
-                                                    >
-                                                        <input className='input_answer'
-                                                            type={quiz.isMultiple ? 'checkbox' : 'radio'}
-                                                            id={`quiz${quiz.id}-answer${answer.id}`}
-                                                            name={`quiz${quiz.id}`}
-                                                            value={answer.id}
-                                                            onChange={() => HandleAnswerClick(quiz.id, answer.id)}
-                                                            checked={selectedAnswers[quiz.id] === answer.id}
-                                                        />
-                                                        <label htmlFor={`quiz${quiz.id}-answer$={answer.id}`}>{answer.content}</label>
-                                                    </li>
-                                                ))
-                                            }
-                                        </ul>
-                                        <button className='clearBtn' onClick={() => ClearSelectedAnswer(quiz.id, SetSelectedAnswers)}>Clear</button>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                        <p className='wrap_btn'>
-                            <button className='btn ClearBtn' onClick={() => ClearAllAnswers(SetSelectedAnswers)}>Clear All Answers</button>
-                            <button className='btn SubmitBtn'>Submit</button>
-                        </p>
+            <h2 className="form__header">Quizz</h2>
+            {props.quizData.lsQuizz && Object.values(props.quizData.lsQuizz).map((quiz) => (
+                <div key={quiz.id}>
+                    <div className="form__body">
+                        <p className="question">Question: {quiz.content}</p>
+                        <ul className="answers" style={{ listStyle: 'none' }}>
+                            {quiz.answer.map((answer) => (
+                                <li className="answer" key={answer.id}>
+                                    <input
+                                        className="input_answer"
+                                        type={quiz.isMultiple ? 'checkbox' : 'radio'}
+                                        id={`quiz${quiz.id}-answer${answer.id}`}
+                                        name={`quiz${quiz.id}`}
+                                        value={answer.id}
+                                        onChange={() => handleAnswerClick(quiz.id, answer.id)}
+                                        checked={selectedAnswers[quiz.id] === answer.id}
+                                    />
+                                    <label htmlFor={`quiz${quiz.id}-answer${answer.id}`}>{answer.content}</label>
+                                </li>
+                            ))}
+                        </ul>
+                        <button className="clearBtn" onClick={() => ClearSelectedAnswer(quiz.id, setSelectedAnswers)}>Clear</button>
                     </div>
-                ))
-            }
-        </div >
-    )
+                </div>
+            ))}
+            <p className="wrap_btn">
+                <button className="btn ClearBtn" onClick={() => ClearAllAnswers(setSelectedAnswers)}>Clear All Answers</button>
+                <button className="btn SubmitBtn">Submit</button>
+            </p>
+        </div>
+    );
 }
--- my-app/src/const/domain.js
+++ my-app/src/const/domain.js
@@ -1,2 +1,2 @@
 export const MAIN_DOMAIN = "localhost:1880";
-export const FETCH_API_DOMAIN = "http://localhost:1880/user"
+export const FETCH_API_DOMAIN = "https://server.nglearns.com/quizz/285498f5-3486-434d-a459-bedb6bcea7ce"
