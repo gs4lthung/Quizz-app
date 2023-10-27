88175b1c8b60e47adfb210a17418c4b63d1722b5
NguyenTuan03 <tuannase170164@fpt.edu.vn>
2023-10-27 14:26:06 +0700

    Fix Giao dien

--- my-app/src/layouts/ScreenHome/HomeService.js
+++ my-app/src/layouts/ScreenHome/HomeService.js
@@ -1,27 +1,21 @@
 import React, { useState } from 'react'
 
-export function HandleInputClick() {
-  const [isactive,SetIsActive] = useState(false);
-  const setInputActive = (e) => {
-    if (e.target.value === '') {
-        SetIsActive(true);
-    }
-    else {
-        SetIsActive(false);
-    }
-  }
-  return {isactive, setInputActive}
+export function Hide(type, setType) {
+    setType(type === 'text' ? 'password' : 'text');
+  }  
+export function HandleInputClick(SetIsActive) {
+    SetIsActive(true);
 }
-
-export function HandleInputBlur() {
-    const [isBlur, SetIsBlur] = useState(false);
-    const setInputBlur = (e) => {
-        if (e.target.value === '') {
-            SetIsBlur(true);
-        }
-        else {
-            SetIsBlur(false);
-        }
+export function HandleInputBlur(SetIsActive, e) {
+    if (e.target.value === '') {
+        SetIsActive(false)
     }
-    return {isBlur,setInputBlur }
+}
+export function InputName(SetisName) {
+    SetisName(true);
+}
+export function BlurName(SetisName, e) {
+    if (e.target.value === '') {
+        SetisName(false)
+    }
 }
--- my-app/src/layouts/ScreenHome/ScreenHome.js
+++ my-app/src/layouts/ScreenHome/ScreenHome.js
@@ -1,30 +1,52 @@
 import React, {useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { FaLock } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash,FaUser } from "react-icons/fa";
 import './Screenhome.scss'
-import { HandleInputBlur, HandleInputClick } from './HomeService';
+import { HandleInputBlur, HandleInputClick, Hide ,InputName,BlurName} from './HomeService.js';
+
 export default function ScreenHome() {
     const nav  = useNavigate();
-    const [type, SetType] = useState(false);
-    const {isactive, setInputActive} = HandleInputClick();
-    const {isBlur, setInputBlur} = HandleInputBlur();
-    const handleHide = () => {
-        SetType(!type)
-    }
+    const [type, SetType] = useState('password');
+    const [isactive,SetIsActive] = useState(false);
+    const [isName,SetisName] = useState(false);
+    const HandleHideClick = () => {
+        Hide(type, SetType);
+    };
+    const HandleClick = () => {
+        HandleInputClick(SetIsActive);
+    };  
+    const HandleBlur = (e) => {
+        HandleInputBlur(SetIsActive, e);
+    };
+    const HandleNameCLick = () => {
+        InputName(SetisName);
+    };  
+    const HandleNameBlur = (e) => {
+        BlurName(SetisName, e);
+    };
     return (
         <div className='container'>
             <div className="form">
                 <div className="group">
                     <h1>Quiz Test</h1>
                 </div>
-                <div class="group-pass">
-                    <i class="fa-solid fa-lock"></i>
-                    <input onBlur={setInputBlur} onClick={setInputActive} type={type ? 'text':'password'} id="password"/>
-                    <i onClick={handleHide} class={`fa-solid ${type ? 'fa-eye': 'fa-eye-slash'}`}></i>
-                    <label class={`name ${isactive && !isBlur ? 'active': ''}`} for="password">Password</label>
+                <div className="group-pass">
+                <FaUser className='icon_1'/>
+                    <input onBlur={HandleNameBlur} onClick={HandleNameCLick} type="text"/>
+                    <label className={`name ${isName ? 'active': ''}`}>Name</label>
                 </div>               
-                <button onClick={() => {
-                    nav('/quiz')
-                }}>Start</button>
+                <div className="group-pass">
+                <FaLock className='icon_1'/>
+                    <input onBlur={HandleBlur} onClick={HandleClick} type={type} id="password"/>
+                    {
+                        type === 'password' ? (<FaEyeSlash onClick={HandleHideClick} className='fa-eye-slash'/>) :
+                        (<FaEye onClick={HandleHideClick} className="fa-eye"/>)
+                    }
+                    <label className={`name ${isactive ? 'active': ''}`} for="password">Password</label>
+                </div>               
+                <button onClick={() => {nav('/quiz')}}>Start</button>
             </div>
         </div>
     )
