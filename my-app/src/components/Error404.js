3d1ff79b6d5b8b0611aa013e65ee53751f4edf8e
Lê Trọng Nhân <kiritomiku0@gmail.com>
2023-10-29 14:44:11 +0700

    Update 404 error

---
+++ my-app/src/components/Error404.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Error404.scss'
+
+export default function Error404() {
+    return (
+        <section className="page_404">
+            <div>
+                <div>
+                    <div>
+                        <div className="text">
+                            <h1>404</h1>
+                            <div className="four_zero_four_bg">
+                                
+                            </div>
+
+                            <div className="contant_box_404">
+                                <h3 className="h2">
+                                    Look like you're lost
+                                </h3>
+
+                                <p>The page you are looking for not avaible!</p>
+
+                                <a href="" className="link_404">Go to Home</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
